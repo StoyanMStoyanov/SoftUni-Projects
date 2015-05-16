6d63fed76ba90a4304b7d1a086c09f97d8c0c571
StoyanMStoyanov <stoyanovengineer@gmail.com>
2015-05-16 15:34:07 +0300

    Commit Strings full homework

--- CSharp Advanced - 04.05.2015/4. Regular Expressions/1. Series of letters/SeriesOfLetters.cs
+++ CSharp Advanced - 04.05.2015/4. Regular Expressions/1. Series of letters/SeriesOfLetters.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Text;
 
@@ -8,21 +7,17 @@ class SeriesOfLetters
     static void Main()
     {
         string letters = "aaaaabbbbbcdddeeeedssaa";
-        string pattern = @"([a-z]+)\1";
+        string pattern = @"(\w)\1*";
         StringBuilder sb = new StringBuilder();
 
         Regex regex = new Regex(pattern);
-        MatchCollection match = regex.Matches(letters);
-        string result = regex.Replace(letters, "*");
-        Console.WriteLine(result);
-        Console.WriteLine(new string('-', 50));
-        foreach (var item in match)
+        MatchCollection matches = regex.Matches(letters);        
+        
+        foreach (var item in matches)
         {
-            Console.WriteLine(item);
-            sb.Append(item.ToString()[0]);
+            //Console.WriteLine(item);
+            Console.Write(item.ToString()[0]);
         }
-        Console.WriteLine(new string('-', 50));
-        Console.WriteLine(sb);
-
+        Console.WriteLine();
     }
 }
