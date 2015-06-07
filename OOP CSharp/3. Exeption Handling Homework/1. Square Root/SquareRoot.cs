276bbb6662c70f18c267af3aa4478177caed1d3a
StoyanMStoyanov <stoyanovengineer@gmail.com>
2015-06-07 13:29:01 +0300

    Commit Exception Handling Homework.

---
+++ OOP CSharp/3. Exeption Handling Homework/1. Square Root/SquareRoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class SquareRoot
+{
+    static void Main()
+    {
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number <= 0) throw new OverflowException();
+            Console.WriteLine(Math.Sqrt(number));
+        }
+        catch (FormatException)
+        {
+            Console.Error.WriteLine("Invalid format. Cannot parse.");
+        }
+        catch (OverflowException)
+        {
+            Console.Error.WriteLine("The number is too long/small, zero or negative. Cannot Parse.");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye.");
+        }
+
+    }
+}
---
+++ OOP CSharp/3. Exeption Handling Homework/2. Enter Numbers/EnterNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class EnterNumbers
+{
+    static void Main()
+    {
+        int start = 1;
+        int end = 100;
+        for (int i = 1; i <= 10; i++)
+        {            
+            Console.WriteLine("{0}  ->  {1}", i, ReadNumbers(start, end));
+        }        
+    }
+
+    private static int ReadNumbers(int start, int end)
+    {        
+        int number = 0;
+        while (true)
+        {
+            try
+            {
+                number = int.Parse(Console.ReadLine());
+                if (number < start || number > end)
+                    throw new ArgumentOutOfRangeException();
+                return number;
+            }
+            catch (FormatException)
+            {
+                Console.Error.WriteLine("Invalid format. Cannot parse.");
+                Console.WriteLine("Try again!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.Error.WriteLine("Argument must be in range [1..100]. ");
+                Console.WriteLine("Try again!");
+            }            
+        }
+    }
+}
