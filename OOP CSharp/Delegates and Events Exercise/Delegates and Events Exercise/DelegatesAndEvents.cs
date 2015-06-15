a0e9f4decf1f1275f7d9f358ea0ecf4c0281d4c2
StoyanMStoyanov <stoyanovengineer@gmail.com>
2015-06-15 23:31:57 +0300

    Commit Delegates and Events Exercise

---
+++ OOP CSharp/Delegates and Events Exercise/Delegates and Events Exercise/DelegatesAndEvents.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class DelegatesAndEvents
+{
+    static void Main()
+    {
+        List<int> collection = new List<int> { 1, 9, 3, 4 , 6, 11, 3, 22};
+        Console.WriteLine(collection.FirstOrDefault(x => x > 7));
+        Console.WriteLine(new string('-', 50));
+        Console.WriteLine(collection.FirstOrDefault(x => x < 0));
+        Console.WriteLine(new string('-', 50));
+        Console.WriteLine(string.Join(", ", collection.TakeWhile(x => x < 10)));
+        Console.WriteLine(new string('-', 50));
+        collection.ForEach(Console.WriteLine);
+        Console.WriteLine(new string('-', 50));
+    }
+}
---
+++ OOP CSharp/Delegates and Events Exercise/Delegates and Events Exercise/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Extensions
+{
+    public static T FirstOrDefault<T>(this IEnumerable<T> collection, Predicate<T> condition)
+    {
+        List<T> result = new List<T>();
+        foreach (var element in collection)
+        {
+            if (condition(element)) return element;
+        }
+        return default(T);
+    }
+
+    public static IEnumerable<T> TakeWhile<T>(this IEnumerable<T> collection,
+        Func<T, bool> predicate)
+    {
+        List<T> result = new List<T>();
+        foreach (var element in collection)
+        {
+            if(!predicate(element)) break;
+            result.Add(element);
+        }
+        return result;
+    }
+
+    public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+    {
+        foreach (var element in collection)         
+            action(element);         
+    }
+}
