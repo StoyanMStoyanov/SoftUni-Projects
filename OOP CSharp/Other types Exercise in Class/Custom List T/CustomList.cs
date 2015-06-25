6ea58998c19e294a73a6600072a3fe7161619550
StoyanMStoyanov <stoyanovengineer@gmail.com>
2015-06-25 06:17:50 +0300

    Commit CustomList Exercise

---
+++ OOP CSharp/Other types Exercise in Class/Custom List T/CustomList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Linq;
+
+class CustomList<T>
+    where T:IComparable<T>
+{
+    private const int DefaultCapacity = 2;
+
+    private T[] elements;
+    private int currentIndex;
+
+    public CustomList(int capacity = DefaultCapacity)
+    {
+        this.elements = new T[capacity];
+        this.currentIndex = 0;
+    }
+
+    public T this[int position]
+    {
+        get
+        {
+            if (position < 0 || position > this.currentIndex)
+                throw new ArgumentOutOfRangeException("position", string.Format("Position should be in the range [0 ... {0}].", this.currentIndex));
+            
+            return this.elements[position];
+        }
+        set
+        {
+            if (position < 0 || position > this.currentIndex)
+                throw new ArgumentOutOfRangeException("position", string.Format("Position should be in the range [0 ... {0}].", this.currentIndex));
+
+            this.elements[position] = value;
+        }
+    }
+
+    //Methods
+    public void Add(T element)
+    {
+        if (this.currentIndex == this.elements.Length)
+            Resize();
+        this.elements[currentIndex] = element;
+        this.currentIndex++;
+    }   
+
+    public void Remove(T element)
+    {
+        for (int index = 0; index < elements.Length; index++)
+        {
+            if (this.elements[index].Equals(element))
+            {
+                this.ReorderElements(index);
+                index--;
+            }
+        }
+    }
+
+    public int IndexOf(T element)
+    {
+        for (int i = 0; i < this.currentIndex; i++)
+        {
+            if (this.elements[i].Equals(element))
+                return i;
+        }
+        return -1;
+    }
+
+    public T Min()
+    {
+        T min = this.elements[0];
+        for (int index = 1; index < this.currentIndex; index++)
+        {
+            T currentElement = this.elements[index];
+            if (currentElement.CompareTo(min) < 0)
+                min = currentElement;
+        }
+        return min;
+    }
+
+    public T Max()
+    {
+        T max = this.elements[0];
+        for (int index = 0; index < this.currentIndex; index++)
+        {
+            T currentElement = this.elements[index];
+            if (currentElement.CompareTo(max) > 0)
+                max = currentElement;
+        }
+        return max;
+    }
+
+    public void Insert(T element, int position)
+    {
+        if (position > this.currentIndex)
+            throw new IndexOutOfRangeException("Index is out of range.");
+        
+        for (int index = this.currentIndex; index > position; index--)
+        {
+            this.elements[index] = this.elements[index - 1];
+        }
+            this.currentIndex++;
+            this.elements[position] = element;
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < currentIndex; i++)
+        {
+            this.elements[i] = default(T);
+        }
+        this.currentIndex = 0;
+    }
+
+    public bool ContainsValue(T element)
+    {
+        for (int index = 0; index < currentIndex; index++)
+        {
+            if (this.elements[index].Equals(element))
+                return true;
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        string temp = string.Join(", ", this.elements.Take(this.currentIndex));
+        return temp;
+    }
+
+    private void Resize()
+    {
+        T[] temp = new T[this.elements.Length * 2];
+        for (int index = 0; index < this.elements.Length; index++)
+        {
+            temp[index] = this.elements[index];
+        }
+        this.elements = temp;
+    }
+
+    private void ReorderElements(int index)
+    {
+        for (int i = index; i < this.currentIndex; i++)
+        {
+            if (i == this.elements.Length-1) break;
+                
+            this.elements[i] = this.elements[i + 1];
+        }
+        this.currentIndex--;
+        this.elements[this.currentIndex] = default(T);        
+    }
+}
---
+++ OOP CSharp/Other types Exercise in Class/Custom List T/CustomListMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class CustomListMain
+{
+    static void Main()
+    {
+        CustomList<int> ints = new CustomList<int>();
+        ints.Add(5);
+        ints.Add(10);
+        ints.Add(20);
+        ints.Add(30);
+        ints.Add(10);
+        ints.Add(10);
+        Console.WriteLine(ints);
+        ints.Remove(10);
+        Console.WriteLine(ints);
+        ints.Add(40);
+        Console.WriteLine(ints);
+        ints.Remove(40);
+        Console.WriteLine(ints);
+        Console.WriteLine(ints.IndexOf(5));
+        Console.WriteLine(ints.Min());
+        Console.WriteLine(ints.Max());
+        Console.WriteLine(ints[1]);
+        ints[1] = 100;
+        Console.WriteLine(ints[1]);
+        Console.WriteLine(ints);
+        ints.Insert(25, 3);
+        Console.WriteLine(ints);
+        Console.WriteLine(ints.ContainsValue(5));
+        //ints[25] = 1;
+    }
+}
